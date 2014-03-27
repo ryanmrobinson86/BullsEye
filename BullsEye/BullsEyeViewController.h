225d81958de4a6c68071ab0418d9be4e286fe5a2
Ryan Robinson <ryanrobinson@Ryan-Robinsons-iMac.local>
2014-03-27 07:42:07 -0400

    A very rough, first take at the project… most features work though.

--- BullsEye/BullsEyeViewController.h
+++ BullsEye/BullsEyeViewController.h
@@ -8,6 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface BullsEyeViewController : UIViewController
+@interface BullsEyeViewController : UIViewController <UIAlertViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UILabel *targetLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *roundLabel;
+
+- (IBAction)showAlert;
+- (IBAction)sliderMoved:(UISlider *)slider;
+- (IBAction)startOver;
+
+- (void)startNewRound;
+- (void)updateLabels;
+- (int)calulateScore;
 
 @end
--- BullsEye/BullsEyeViewController.m
+++ BullsEye/BullsEyeViewController.m
@@ -13,17 +13,96 @@
 @end
 
 @implementation BullsEyeViewController
+{
+    int _currentValue;
+    int _targetValue;
+    int _score;
+    int _round;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self startOver];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    [self startNewRound];
+}
+
+- (IBAction)showAlert
+{
+    int points = [self calulateScore];
+    NSString *message;
+    NSString *title;
+    
+    if(points == 100){
+        title = @"Perfect!";
+        points += 100;
+    }else if(points >= 95){
+        title = @"Almost There";
+        points += 50;
+    }else if(points >= 80){
+        title = @"Close";
+    }else if(points >= 50){
+        title = @"Fine, but...";
+        points -= 25;
+    }else{
+        title = @"C'mon now";
+        points = 0;
+    }
+    
+    message = [NSString stringWithFormat:@"You Scored %d Points", points];
+    
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title message:message delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    _score += points;
+    _round += 1;
+    
+    [alertView show];
+}
+
+- (IBAction)sliderMoved:(UISlider *)slider
+{
+    _currentValue = lroundf(slider.value);
+}
+
+- (IBAction)startOver
+{
+    _score = 0;
+    _round = 1;
+    [self startNewRound];
+}
+
+- (void)startNewRound
+{
+    _currentValue = self.slider.value;
+    _targetValue = arc4random_uniform(100);
+    
+    [self updateLabels];
+}
+
+- (void)updateLabels
+{
+    self.targetLabel.text = [NSString stringWithFormat:@"%d", _targetValue];
+    self.roundLabel.text = [NSString stringWithFormat:@"%d", _round];
+    self.scoreLabel.text = [NSString stringWithFormat:@"%d", _score];
+}
+
+- (int)calulateScore
+{
+    int difference = abs(_targetValue - _currentValue);
+    
+    return lround(100 - difference);
+}
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    [self startNewRound];
 }
 
 @end

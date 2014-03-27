8ee6fd4e8c1181f5734faaef6ed69ed9885fe492
Ryan Robinson <ryan.macmaster.robinson@gmail.com>
2014-03-27 18:08:30 -0400

    finished designing the app, now onto making it look pretty.

---
+++ BullsEye/BullsEyeAboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  BullsEyeAboutViewController.h
+//  BullsEye
+//
+//  Created by Ryan Robinson on 3/27/14.
+//  Copyright (c) 2014 RRobinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BullsEyeAboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
